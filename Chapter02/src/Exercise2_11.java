f6df9b6f78ac8870127393f44c84d75157ffba68
Oybek Turaev <turaev_o@auca.kg>
2020-10-15 22:01:30 +0500

    Updating

--- Chapter02/src/Exercise2_11.java
+++ Chapter02/src/Exercise2_11.java
@@ -12,17 +12,21 @@ public class Exercise2_11 {
          *  Write a program to display the population for each of the next five years
          */
 
+        final int CURRENT_POPULATION = 312_032_486;
+
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter the number of years: ");
-        int years = scanner.nextInt();
-        int current_population = 312_032_486;
+        int number_years = scanner.nextInt();
 
-        double birth_rate = years * ((365 * 24 * 60 * 60) / 7.0);
-        double death_rate = years * ((365 * 24 * 60 * 60) / 13.0);
-        double immigrant_rate = years * ((365 * 24 * 60 * 60) / 45.0);
+        double birth_rate = number_years * ((365 * 24 * 60 * 60) / 7.0);
+        double death_rate = number_years * ((365 * 24 * 60 * 60) / 13.0);
+        double immigrant_rate = number_years * ((365 * 24 * 60 * 60) / 45.0);
 
-        double population_in_five_years = current_population + (birth_rate + immigrant_rate - death_rate);
+        double population_in_years = CURRENT_POPULATION + (birth_rate + immigrant_rate - death_rate);
 
-        System.out.println("The population in " + years + " years is " + (int)population_in_five_years);
+        System.out.printf("The population in %d years is %.0f%n", number_years, population_in_years);
+
+        //System.out.println("The population in " + years + " years is " + (int)population_in_five_years);
     }
 }
