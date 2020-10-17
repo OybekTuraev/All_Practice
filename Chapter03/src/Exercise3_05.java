8d8a661ea3e5750e1049583828cce946074464fd
Oybek Turaev <turaev_o@auca.kg>
2020-10-17 15:50:32 +0500

    Finish Exercises in Chapter 3

--- Chapter03/src/Exercise3_05.java
+++ Chapter03/src/Exercise3_05.java
@@ -1,2 +1,53 @@
+import java.util.Scanner;
+
 public class Exercise3_05 {
+    public static void main(String[] args){
+
+        /**
+         * Find future dates
+         */
+
+        Scanner scanner  = new Scanner(System.in);
+
+        System.out.print("Enter today's day: ");
+        int day_num = scanner.nextInt();
+        System.out.print("Enter the number of days elapsed since today: ");
+        int number = scanner.nextInt();
+
+        int future_day = (number + day_num) % 7;
+
+        switch (day_num){
+            case 0:
+                System.out.print("Today is Sunday"); break;
+            case 1:
+                System.out.print("Today is Monday"); break;
+            case 2:
+                System.out.print("Today is Tuesday"); break;
+            case 3:
+                System.out.print("Today is Wednesday"); break;
+            case 4:
+                System.out.print("Today is Thursday"); break;
+            case 5:
+                System.out.print("Today is Friday"); break;
+            case 6:
+                System.out.print("Today is Saturday");
+        }
+
+        switch (future_day){
+            case 0:
+                System.out.println(" and the future day is Sunday"); break;
+            case 1:
+                System.out.println(" and the future day is Monday"); break;
+            case 2:
+                System.out.println(" and the future day is Tuesday"); break;
+            case 3:
+                System.out.println(" and the future day is Wednesday"); break;
+            case 4:
+                System.out.println(" and the future day is Thursday"); break;
+            case 5:
+                System.out.println(" and the future day is Friday"); break;
+            case 6:
+                System.out.println(" and the future day is Saturday");
+        }
+    }
 }
