403dcb5d09c37f744ee2ffdc081343c36acd9812
Oybek Turaev <turaev_o@auca.kg>
2020-10-17 10:48:29 +0500

    Finish Exercises in Chapter 3

---
+++ Chapter03/src/Exercise3_03.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Exercise3_03 {
+    public static void main(String[] args){
+
+        /**
+         * Algebra: solve 2 * 2 linear equations
+         *  By Cramer's rule
+         */
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a, b, c, d, e, f : ");
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+        double d = scanner.nextDouble();
+        double e = scanner.nextDouble();
+        double f = scanner.nextDouble();
+
+        if (a * d - b * c == 0){
+            System.out.println("The equation has no solution");
+        }
+        else{
+            double x = (e * d - b * f) / (a * d - b * c);
+            double y = (a * f - e * c) / (a * d - b * c);
+            System.out.println("x is " + x + " and y is " + y);
+        }
+    }
+}
