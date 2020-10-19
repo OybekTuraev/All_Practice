d6c7ebbc004b0f8c496ffd798257e337c745becf
Oybek Turaev <turaev_o@auca.kg>
2020-10-20 01:55:59 +0500

    Finish Problems in Chapter 3

--- Chapter03/src/Exercise3_34.java
+++ Chapter03/src/Exercise3_34.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-public class Exercise3_32 {
-    public static void main(String[] args) {
+public class Exercise3_34 {
+    public static void main(String[] args){
 
         /**
-         *  Geometry: point position
+         *  Geometry: point on line segment
          */
 
         Scanner scanner = new Scanner(System.in);
@@ -21,18 +21,12 @@ public class Exercise3_32 {
         double x2 = scanner.nextDouble();
         double y2 = scanner.nextDouble();
 
-        double formula = (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0);
-
-        if (formula > 0){
-            System.out.printf("(%.1f, %.1f) is on the left side of the line from (%.1f, %.1f) to (%.1f, %.1f)%n", +
-                    x2, y2, x0, y0, x1, y1);
-        }
-        else if (formula == 0){
-            System.out.printf("(%.1f, %.1f) is on the line from (%.1f, %.1f) to (%.1f, %.1f)%n", +
+        if (((x2 > x0) && (x2 < x1)) && ((y2 > y0) && (y2 < y1)) ){
+            System.out.printf("(%.1f, %.1f) is on the line segment from (%.1f, %.1f) to (%.1f, %.1f)%n", +
                     x2, y2, x0, y0, x1, y1);
         }
         else{
-            System.out.printf("(%.1f, %.1f) is on the right side of the line from (%.1f, %.1f) to (%.1f, %.1f)%n", +
+            System.out.printf("(%.1f, %.1f) is not on the line segment from (%.1f, %.1f) to (%.1f, %.1f)%n", +
                     x2, y2, x0, y0, x1, y1);
         }
     }
