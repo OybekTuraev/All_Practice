f3a97dc49bee45de1bd1ec89bc1050614b6702a8
Turaev Oybek <turaev_o@auca.kg>
2020-11-30 22:18:56 +0500

    test

--- JavaGrapics/src/G09.java
+++ JavaGrapics/src/G09.java
@@ -9,10 +9,22 @@ public class G09 extends PApplet {
 
     public void setup(){
 
+        background(0);
+
+        textSize(16);
+        //textAlign(RIGHT);
+        text("ABCD", 50, 30);
+
+       //textAlign(CENTER);
+        text("MMMM", 50, 50);
+
+        //textAlign(LEFT);
+        text("KKKK", 50, 70);
+
+
     }
 
     public void draw(){
-
     }
 
 
