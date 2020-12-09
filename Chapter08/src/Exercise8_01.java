87dd95dcbf100f62d22e1544ae355a59ee524a0a
Turaev Oybek <turaev_o@auca.kg>
2020-12-10 04:42:30 +0500

    Chapter 8 Exercises

---
+++ Chapter08/src/Exercise8_01.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Exercise8_01 {
+    public static void main(String[] args) {
+
+        // Sum elements column by column
+
+        double[][] array = readAInput();
+
+        showSum(array);
+    }
+
+    public static double[][] readAInput(){
+        final int ROW = 3;
+        final int COLUMN = 4;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Enter a %d-by-%d matrix row by row: %n", ROW, COLUMN);
+
+        double[][] array = new double[ROW][COLUMN];
+
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < array[i].length; j++){
+
+                array[i][j] = scanner.nextDouble();
+            }
+        }
+
+        return array;
+    }
+
+    public static void showSum(double[][] array){
+
+        for (int column = 0; column < array[0].length; column++){
+            System.out.println("Sum of the elements at column " + column + " is " + sumColumn(array, column));
+        }
+    }
+
+    public static double sumColumn(double[][] array, int column){
+
+        double sum = 0;
+        for (int row = 0; row < array.length; row++) {
+                sum += array[row][column];
+        }
+
+        return sum;
+    }
+}
