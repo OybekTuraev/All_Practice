a9532c1d9fc2f84bef9c94e4761de729f43cb99f
Turaev Oybek <turaev_o@auca.kg>
2021-05-07 08:52:50 +0500

    New object of GUI

---
+++ MineSweeper/src/Field.java
@@ -0,0 +1,10 @@
+public class Field {
+    private final int MINES_FOR_BEGINNER     = 10;
+    private final int MINES_FOR_INTERMEDIATE = 40;
+    private final int MINES_FOR_EXPERT       = 99;
+
+
+
+
+
+}
