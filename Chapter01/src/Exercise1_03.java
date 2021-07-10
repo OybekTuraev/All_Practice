d5c66420050ef49fd17fb2a3722b4761e36aa6e4
Oybek Turaev <commuter0706@gmail.com>
2021-07-10 16:12:54 +0500

    Check

--- Chapter01/src/Exercise1_03.java
+++ Chapter01/src/Exercise1_03.java
@@ -9,5 +9,7 @@ public class Exercise1_03 {
         System.out.println("     J      A A      V   V      A A     ");
         System.out.println("J    J     AAAAA      V V      AAAAA    ");
         System.out.println("  JJ      A     A      V      A     A   ");
+
+        System.out.println("Check the Git Bash");
     }
 }
